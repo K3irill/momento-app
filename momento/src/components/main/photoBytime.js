6aa9b0c277f44614fb4e646070b3bba1af6ca65e
K3rill <razdor.2019@mail.ru>
2024-09-19 21:32:58 +0300

    change path to files

--- momento/src/components/main/photoBytime.js
+++ momento/src/components/main/photoBytime.js
@@ -2,21 +2,21 @@ export const timeOfDay = [
   {
     title: "night",
     time_range: ["00:00", "06:00"],
-    url: "/assets/for-slider/04.jpg",
+    url: "./assets/for-slider/04.jpg",
   },
   {
     title: "morning",
     time_range: ["6:00", "12:00"],
-    url: "/assets/for-slider/01.jpg",
+    url: "./assets/for-slider/01.jpg",
   },
   {
     title: "afternoon",
     time_range: ["12:00", "18:00"],
-    url: "/assets/for-slider/02.jpg",
+    url: "./assets/for-slider/02.jpg",
   },
   {
     title: "evening",
     time_range: ["18:00", "00:00"],
-    url: "/assets/for-slider/03.jpg",
+    url: "./assets/for-slider/03.jpg",
   },
 ];
