00833f7f591ff22ea1d576fc5a825cbb687b6bed
K3rill <razdor.2019@mail.ru>
2024-09-19 20:37:06 +0300

    favicon

--- momento/src/main.js
+++ momento/src/main.js
@@ -1,6 +1,5 @@
 import "./styles/global.scss";
 import { addElementsToBlock, renderHeader } from "./components/header/header";
-import { renderTimePage } from "./components/main/time-page/time-page";
 import { renderMain } from "./components/main/main-block";
 
 document.addEventListener("DOMContentLoaded", async () => {
